a4abcb6d6d49e5080b13e45ad0140db73debcae7
zerasul <zerasul@gmail.com>
2022-11-17 18:33:03 +0100

    exercise 1

--- src/main/java/com/spartacommodities/interviews/sudoku/SudokuBoard.java
+++ src/main/java/com/spartacommodities/interviews/sudoku/SudokuBoard.java
@@ -1,5 +1,7 @@
 package com.spartacommodities.interviews.sudoku;
 
+import java.util.*;
+
 public class SudokuBoard {
     private final int[][] board;
 
@@ -9,6 +11,51 @@ public class SudokuBoard {
 
     // TODO add implementation
     public boolean isSolved() {
-        return true;
+
+        boolean checkValueFlag =true;
+        List<Set<Integer>> rows = new ArrayList<>();
+
+        for (int i =0;i< board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j]<1 || board[i][j]>9){
+                    checkValueFlag=false;
+                    break;
+                }
+            }
+        }
+
+        for (int i =0;i< board.length;i++){
+            Set<Integer> row = new HashSet<>();
+            for(int j=0;j<board[i].length;j++){
+               row.add(board[i][j]);
+            }
+            rows.add(row);
+        }
+        boolean checkuniqueRows = rows.stream().noneMatch(integers -> integers.size()<9);
+
+
+        List<Set<Integer>> cols = new ArrayList<>();
+        for(int i = 0; i<board.length;i++){
+            Set<Integer> col = new HashSet<>();
+            for(int j=0;j<board[i].length;j++) {
+                col.add(this.board[j][i]);
+            }
+            cols.add(col);
+        }
+        boolean checkUniqueCols = cols.stream().noneMatch(integers -> integers.size()<9);
+
+        List<Set<Integer>> squares = new ArrayList<>();
+
+        for(int z= 0;z<9;z++ ) {
+            for (int i = 0; i < 3; i++) {
+                Set<Integer> col = new HashSet<>();
+                for (int j = 0; j < 3; j++) {
+                    col.add(this.board[i][j]);
+                }
+                squares.add(col);
+            }
+        }
+
+        return checkValueFlag && checkuniqueRows && checkUniqueCols;
     }
 }
